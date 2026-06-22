129b2eb000b7d6c4c92e8ced23103cb6fe9814f0
Desai <kolobokevgenij83@gmail.com>
2026-06-22 05:25:49 +0300

    feat: export api modules from shared index file

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,5 +1,5 @@
 export { apiClient } from './base'
 export { authApi } from './auth.api'
 export { customerApi } from './customer.api'
-export { merchantApi } from './merchant.api'
+export { merchantApi, type MerchantListResponse } from './merchant.api'
 export { uploadApi } from './upload.api'
